73b87e49157d76ac2543739ac27de9689f75e291
Martin <1822586@hs-mannheim.de>
2020-03-24 15:46:38 +0100

    Ausprobieren merg

---
+++ GitTestProjekt/src/testing/testenMerg.java
@@ -0,0 +1,13 @@
+package testing;
+
+public class testenMerg {
+
+	public static void main(String[] args) {
+		int zahl=0;
+		String  [] dudes = {"Jonny","Michael","Maddin"};
+		String dude = dudes[zahl];
+		zahl = 1;
+		System.out.println(dude);
+	}
+
+}
