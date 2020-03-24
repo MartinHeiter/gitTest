c3ead2a8aae18e686feb70df1f2b4af94db063a2
Martin <1822586@hs-mannheim.de>
2020-03-24 16:03:17 +0100

    testAgain

---
+++ GitTestProjekt/src/testing/weitererTest.java
@@ -0,0 +1,12 @@
+package testing;
+
+public class weitererTest {
+
+	private int anzahl;
+	private String Werte;
+	
+	weitererTest(int anzahl, String Werte){
+		this.anzahl = anzahl;
+		this.Werte = Werte;
+	}
+}
